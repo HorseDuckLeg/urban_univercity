e44bb122b1b2bff8635247297ee50e769153acc8
HorseDuckLeg <msksia@yandex.ru>
2024-05-29 11:24:48 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,5 @@
-
+compl_tasks = 12                                          #количество выполненных ДЗ
+waste_hours = 1.5                                         #количество затраченных часов
+course_name = 'Python'                                    #название курса
+time_of_task = waste_hours/compl_tasks                    #время на одно задание
+print("Курс:",course_name,", всего задач:",compl_tasks,", затрачено часов:",waste_hours,", среднее время выполнения",time_of_task," часа")
