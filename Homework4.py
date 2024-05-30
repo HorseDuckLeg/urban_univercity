bc09714b7d81b8ef77fb1733267f2a397a1ca7ec
HorseDuckLeg <msksia@yandex.ru>
2024-05-30 14:07:12 +0300

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -1,1 +1,9 @@
-
+#1
+my_string = input('Введите произвольный текст: ')
+print('Количество символов которое Вы ввели = ',len(my_string))
+#2
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0:1:])
+print(my_string[-1::])
