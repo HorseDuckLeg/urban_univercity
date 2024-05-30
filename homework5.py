8a3b6e698100fcab1a9c7d71f48ab552110d1ebf
HorseDuckLeg <msksia@yandex.ru>
2024-05-30 14:07:58 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,1 +1,7 @@
-
+immutable_var = ('Кортеж', 1, 2, 3, 5.5, False)
+print('Immutable tuple: ',immutable_var)
+mutable_list = ["Список", 1,2,3,'a','b','c']
+print('Mutable list before modified: ',mutable_list)
+mutable_list.append('Modified')
+mutable_list.extend('qwerty')
+print('Mutable list after modified: ',mutable_list)
