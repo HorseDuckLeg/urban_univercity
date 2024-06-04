924464a6f4e2d97796c6215593ee62ebbca7c063
HorseDuckLeg <msksia@yandex.ru>
2024-06-04 14:27:10 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,1 +1,9 @@
-
+first = int(input('Введите первое число:'))
+second = int(input('Введите второе число:'))
+thrid = int(input('Введите третье число:'))
+if first == second and second == thrid:
+    print('3')
+elif first == second or first == thrid or second == thrid:
+    print('2')
+else:
+    print('0')
