3a174bf6ae0bfcc8d65d555685d5a78eecaac0c5
HorseDuckLeg <msksia@yandex.ru>
2024-06-10 20:26:01 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,1 +1,24 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+
+def is_prime(i):
+    for j in range(2, (i // 2 + 1)):
+        if i % j == 0:
+            return False
+    return True
+
+
+for i in numbers:
+    if i >= 2:
+        if is_prime(i):
+            primes.append(i)
+        else:
+            not_primes.append(i)
+
+print(primes)
+print(not_primes)
+
+
 
