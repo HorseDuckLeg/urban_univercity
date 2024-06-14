97e8457a9a2c2f5808c3c194ddd88e5f23f0760e
HorseDuckLeg <msksia@yandex.ru>
2024-06-14 15:48:22 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,11 +3,11 @@
 #1218273645 - нужный пароль (1 и 2, 1 и 8, 2 и 7, 3 и 6, 4 и 5 - пары; число 9 кратно сумме каждой пары)
 #
 #first_insert - первая вставка
-#second_insert - вторая вставка
 #first_insert_fu - функция для отпределения случайного числа первой вставки в диапазоне 3-20
-#pair_part_1 - первая часть пары
-#pair_part_2 - вторая часть пары
+#pair_part_1 - числовой ряд от 1 до случайного числа из первой вставки
+#pair_part_2 - список пар во второй вставке
 #password - пароль для числа из первой вставки
+#result - результат
 
 import random
 
@@ -18,34 +18,30 @@ def first_insert_fu():
     return first_insert
 
 
-print('Число из первой вставки: ', first_insert_fu())
+n = first_insert_fu()
+#print('Число из первой вставки: ', n)
 pair_part_1 = []
 pair_part_2 = []
-#m = first_insert_fu()
-for c in range(first_insert_fu()):
-    a = c + 1  # присваиваем переменной число что бы начать перебор с 1 для первой части пары
-    print('тест - выводим значение a:', a)
-    pair_part_1.append(a)  # записываем число в первую часть пары
-    print('числа из первой части парыЖ', pair_part_1)
-    for i in range(first_insert_fu()):
-        print('тест - выводим значение i:', i)
-        if first_insert_fu() % (a + (i + 1)) == 0:
-            print('Остаток от деления ноль, сумма пары кратна')
-            b = (i + 1) #записываем значение второй части пары удовлетворяющее условиям кратности
-            pair_part_2.append(b)
-            print('числа из второй части пары: ', pair_part_2)
-        else:
-            print('Остаток от деления не равен нулю, либо первая часть пары больше половины числа нет смысла вести дальше перебор')
-            continue
-print(first_insert_fu())
-print('список первой части пар:', pair_part_1)
-print('список второй части пар:', pair_part_2)
+password = []
+for c in range(1, n + 1):
+    pair_part_1.append(c)
+    #print('pair_part_1', pair_part_1)
 for i in pair_part_1:
-    password.append(i)
-    for j in pair_part_2:
-        if first_insert_fu() % (pair_part_1[i] + pair_part_2[j]):
-            password.append(j)
-        else:
-            continue
+    if i <= ((n / 2) + 1):
+        for j in pair_part_1:
+            if j >= i:
+                if n % (i + j) == 0:
+                    pair_part_2.append(i)
+                    pair_part_2.append(j)
+                    #print('pair_part_2', pair_part_2)
+                else:
+                    #print('Сумма не кратна', n)
+                    continue
 
-print('Код для числа:', first_insert_fu(), ' : ', password)
+print(n, ' - Число из первой вставки')
+#print('список для перебора от 1 до числа из первой вставки:', pair_part_1)
+#print('список пар сумма которых кратна числу из первой вставки:', pair_part_2)
+password = (pair_part_2)
+print(password)
+result = "".join(str(i) for i in password)
+print('Нужный пароль:', result)
