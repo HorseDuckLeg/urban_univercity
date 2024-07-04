022a663cc3f6a3d4cabf5529a0448f4ade5f43f3
HorseDuckLeg <msksia@yandex.ru>
2024-07-04 18:32:46 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,1 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)            #переводим значение number из числового в строковое
+    first = int(str(str_number[0]))     #забираем первый символ из str_number
+    len_number = len(str_number)        #измеряем количество цифр в нашем числе
+    if len_number == 1:                 #если число состоит из одной цифры, выводим сразу это число
+        return first
+    else:                               #рекурсивно вычисляем произведение  всех цифр числа
+        return first * get_multiplied_digits(int(str_number[1:]))
 
+
+
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(3))
