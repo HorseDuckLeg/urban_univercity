4e411e7aed94ab4969dba8292251f236e875607d
HorseDuckLeg <msksia@yandex.ru>
2024-07-23 17:07:34 +0300

    Update module_5_4.py

--- module_5_4.py
+++ module_5_4.py
@@ -12,110 +12,123 @@
 class House:
 
     houses_history = []
-    __instance = None
+    #__instance = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls[House.__init__()])
-        return cls.__instance
+    def __new__(cls, *args):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
 
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
+    def __init__(self, *args):
+        self.name = args[0]
+        self.number_of_floors = args[1]
 
-    def go_to(self, new_floor):
-        if new_floor > self.number_of_floors or new_floor < 1:
-            print('Такого этажа не существует')
-        else:
-            for i in range(1, new_floor + 1):
-                print(i)
+    def __del__(self):
+        print(self.name, "снесён, но останется в истории.")
+        return self
 
-    def __len__(self):
-        return self.number_of_floors
+#     def go_to(self, new_floor):
+#         if new_floor > self.number_of_floors or new_floor < 1:
+#             print('Такого этажа не существует')
+#         else:
+#             for i in range(1, new_floor + 1):
+#                 print(i)
+#
+#     def __len__(self):
+#         return self.number_of_floors
+#
+# #    def __str__(self):
+# #        return f'{self.name}, {self.number_of_floors}'
+#
+#     def __eq__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors == other
+#         elif isinstance(other, House):
+#             return self.number_of_floors == other.number_of_floors
+#         else:
+#             return f'object "Other" in __eq__ is not INT or Class "House" type'
+#
+#     def __lt__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors < other
+#         elif isinstance(other, House):
+#             return self.number_of_floors < other.number_of_floors
+#         else:
+#             return f'object "Other" in __lt__ is not INT or Class "House" type'
+#
+#     def __le__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors <= other
+#         elif isinstance(other, House):
+#             return self.number_of_floors <= other.number_of_floors
+#         else:
+#             return f'object "Other" in __le__ is not INT or Class "House" type'
+#
+#     def __gt__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors > other
+#         elif isinstance(other, House):
+#             return self.number_of_floors > other.number_of_floors
+#         else:
+#             return f'object "Other" in __gt__ is not INT or Class "House" type'
+#
+#     def __ge__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors >= other
+#         elif isinstance(other, House):
+#             return self.number_of_floors >= other.number_of_floors
+#         else:
+#             return f'object "Other" in __ge__ is not INT or Class "House" type'
+#
+#     def __ne__(self, other):
+#         if isinstance(other, int):
+#             return self.number_of_floors != other
+#         elif isinstance(other, House):
+#             return self.number_of_floors != other.number_of_floors
+#         else:
+#             return f'object "Other" in __ne__ is not INT or Class "House" type'
+#
+#     def __add__(self, value):
+#         if isinstance(value, int):
+#             self.number_of_floors += value
+#             return self
+#         else:
+#             return f'object "Value" in __add__ is not INTor Class "House" type'
+#
+#     def __radd__(self, value: int):
+#         if isinstance(value, int):
+#             self.number_of_floors += value
+#             return self
+#         else:
+#             return f'object "Value" in __radd__ is not INT or Class "House" type'
+#
+#     def __iadd__(self, other: int):
+#         if isinstance(other, int):
+#             self.number_of_floors += other
+#             return self
+#         else:
+#             return f'object "Value" in __iadd__ is not INT or Class "House" type'
 
-    def __str__(self):
-        return f'{self.name}, {self.number_of_floors}'
-
-    def __eq__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors == other
-        elif isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
-        else:
-            return f'object "Other" in __eq__ is not INT or Class "House" type'
-
-    def __lt__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors < other
-        elif isinstance(other, House):
-            return self.number_of_floors < other.number_of_floors
-        else:
-            return f'object "Other" in __lt__ is not INT or Class "House" type'
-
-    def __le__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors <= other
-        elif isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-        else:
-            return f'object "Other" in __le__ is not INT or Class "House" type'
-
-    def __gt__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors > other
-        elif isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
-        else:
-            return f'object "Other" in __gt__ is not INT or Class "House" type'
-
-    def __ge__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors >= other
-        elif isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
-        else:
-            return f'object "Other" in __ge__ is not INT or Class "House" type'
-
-    def __ne__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors != other
-        elif isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-        else:
-            return f'object "Other" in __ne__ is not INT or Class "House" type'
-
-    def __add__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
-            return self
-        else:
-            return f'object "Value" in __add__ is not INTor Class "House" type'
-
-    def __radd__(self, value: int):
-        if isinstance(value, int):
-            self.number_of_floors += value
-            return self
-        else:
-            return f'object "Value" in __radd__ is not INT or Class "House" type'
-
-    def __iadd__(self, other: int):
-        if isinstance(other, int):
-            self.number_of_floors += other
-            return self
-        else:
-            return f'object "Value" in __iadd__ is not INT or Class "House" type'
 
 
 
-
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
+# h1 = House('ЖК Эльбрус', 10)
+# h2 = House('ЖК Акация', 20)
+# h3 = House('ЖК Матрёшки', 20)
+####
 h1 = House('ЖК Эльбрус', 10)
 print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+#print(House.__mro__)
+#print(h1.__dict__)
 
+# Удаление объектов
+del h2
+del h3
 
-# print(h1)
-# print(h2)
+print(House.houses_history)
 #
 # print(h1 == h2) # __eq__
 # h1 = h1 + 10 # __add__
